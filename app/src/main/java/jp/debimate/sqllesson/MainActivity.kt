2fed985f0772a9d6ed44f7ecad17c76a148a6bbc
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2022-07-16 11:20:26 +0900

    First commit

---
+++ app/src/main/java/jp/debimate/sqllesson/MainActivity.kt
@@ -0,0 +1,11 @@
+package jp.debimate.sqllesson
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
